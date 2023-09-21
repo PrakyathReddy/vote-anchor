73b3f6438ead2f6e66b596633767c57d79bb1825
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-21 09:01:50 +0530

    added upvote and downvote functions

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # vote-anchor
 
 In this program, we are going to take some content from the internet - any url or any content. We are going to hash that content, and create a unique account for that content and people are going to be able to upvote or downvote on that content in a completely decentralized way.
+
+We are going to have 3 functions:
+1. Initialize
+2. Upvote
+3. Downvote
--- programs/vote-anchor/src/lib.rs
+++ programs/vote-anchor/src/lib.rs
@@ -6,10 +6,58 @@ declare_id!("GAutBpYRWyKY6pARFU78JR2akq9GMBL2c1Goish5PzCc");
 pub mod vote_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote.votes = 0;
+        ctx.accounts.vote.bump = *ctx.bumps.get("vote").unwrap(); // unwrap because 'get' returns an option
+        Ok(())
+    }
+
+    pub fn upvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote.votes += 1;
+        Ok(())
+    }
+
+    pub fn downvote(ctx: Context<VoteInteraction>, _hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.vote.votes -= 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(_hash: Vec<u8>)] // by using instruction, we are exposing the hash into our accounts atruct, inside the context
+pub struct Initialize<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, // we need someone to pau for the account
+    #[account(
+        init,
+        payer = signer,
+        space = Vote::LEN,
+        seeds = [b"vote", _hash.as_slice().as_ref()], // seeds are going to be set to seeds ("vote") + this hash
+        bump // here, it is going to calculate the bump for us. Save it using the initialize instruction
+    )]
+    vote: Account<'info, Vote>, // going to hold the number of votes
+    system_program: Program<'info, System>, // since we are creating an account, we need the system program
+}
+
+#[derive(Accounts)]
+#[instruction(_hash: Vec<u8>)]
+pub struct VoteInteraction<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, // we need someone to pau for the account
+    #[account(
+        mut,
+        seeds = [b"vote", _hash.as_slice().as_ref()], // seeds are going to be set to seeds ("vote") + this hash
+        bump = vote.bump, // reusing the saved bump
+    )]
+    vote: Account<'info, Vote>, // going to hold the number of votes
+}
+
+#[account]
+pub struct Vote {
+    votes: i64,
+    bump: u8,
+}
+
+impl Vote {
+    const LEN: usize = 8 + 8 + 1; // 8 bytes for the anchor discriminator, 8 bytes for the votes, 1 byte for the bump
+}
